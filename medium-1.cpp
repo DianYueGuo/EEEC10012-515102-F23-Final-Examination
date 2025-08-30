6cd7a63f414fb919ea4aa794bd6db6701f9d0362
DianYueGuo <dianyueguo@gmail.com>
2025-08-30 21:14:33 +0800

    simplify to only four lines of code

--- medium-1.cpp
+++ medium-1.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <string>
 
-void process_cli_arguments(
-    const char* const argv[],
-    std::string &input_file_name,
-    std::string &task1_out_file_name,
-    std::string &task2_out_file_name,
-    std::string &task3_out_file_name
-);
-
 int main(int argc, char *argv[]) {
 
     if (argc != 1 + 4) {
@@ -16,25 +8,10 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    std::string input_file_name;
-    std::string task1_out_file_name;
-    std::string task2_out_file_name;
-    std::string task3_out_file_name;
-
-    process_cli_arguments(argv, input_file_name, task1_out_file_name, task2_out_file_name, task3_out_file_name);
+    std::string input_file_name(argv[1]);
+    std::string task1_out_file_name(argv[2]);
+    std::string task2_out_file_name(argv[3]);
+    std::string task3_out_file_name(argv[4]);
 
     return 0;
-}
-
-void process_cli_arguments(
-    const char* const argv[],
-    std::string &input_file_name,
-    std::string &task1_out_file_name,
-    std::string &task2_out_file_name,
-    std::string &task3_out_file_name
-) {
-    input_file_name.assign(argv[1]);
-    task1_out_file_name.assign(argv[2]);
-    task2_out_file_name.assign(argv[3]);
-    task3_out_file_name.assign(argv[4]);
 }
